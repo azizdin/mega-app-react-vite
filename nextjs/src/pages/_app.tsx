e4ecdd80d97d1de0a93523378a39a2989ca75721
Abdul Aziz Bin Din <azizdin395@gmail.com>
2023-05-11 22:51:35 +0800

    fix global style import

--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import "@/common/styles/globals.css";
+import "@common/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+``;
