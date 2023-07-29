e98da8440b041180e9cbbe05c353d8562b3128a5
Azzumar Aydid <50803504+azzumaraydid7@users.noreply.github.com>
2023-07-29 17:02:56 +0800

    update (#11)
    * update
    
    * Delete .env

---
+++ nuxt/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
